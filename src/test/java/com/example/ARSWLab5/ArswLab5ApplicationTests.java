b072faad7bc6546718ddb706e98ac3180cabc7f0
arenas <julian.arenas-a@mail.escuelaing.edu.co>
2026-03-17 00:12:28 -0500

    Subiendo el pom.xml y archivos faltantes para el despliegue

---
+++ src/test/java/com/example/ARSWLab5/ArswLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ARSWLab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArswLab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
